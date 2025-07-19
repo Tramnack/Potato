98895ae39d8ebe6c5efb9a6b95fbccc4cf057303
Josef Rittiner <josef.rittiner@stud.hslu.ch>
2025-07-19 23:30:01 +0200

    Updated RabitMQConsumer.py - Made `RabitMQConsumer` an Abstract Class - Added Abstract Method `callback` - Removed `callback` arg from `consume`

--- services/shared_libs/RabbitMQ/RabbitMQConsumer.py
+++ services/shared_libs/RabbitMQ/RabbitMQConsumer.py
@@ -1,19 +1,21 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
-from pika.exceptions import ChannelClosedByBroker, ChannelClosed
+from pika.channel import Channel
+from pika.spec import Basic, BasicProperties
 
 from services.shared_libs.RabbitMQ.AbstractRabbitMQ import AbstractRabbitMQ
 
 
-class RabbitMQConsumer(AbstractRabbitMQ):
-    def setup(self):
-        # Optionally declare queue or bindings
-        pass
+class RabbitMQConsumer(AbstractRabbitMQ, ABC):
 
-    def consume(self, queue: str, callback, auto_ack: bool = False):
+    def consume(self, queue: str, auto_ack: bool = False):
         self.channel.queue_declare(queue=queue, durable=True)
         self.channel.basic_qos(prefetch_count=1)
-        self.channel.basic_consume(queue=queue, on_message_callback=callback, auto_ack=auto_ack)
+        self.channel.basic_consume(queue=queue, on_message_callback=self.callback,  auto_ack=auto_ack)
         print(f"Consuming messages from queue '{queue}'...")
         self.channel.start_consuming()
 
+    @abstractmethod
+    def callback(self, ch: Channel, method: Basic.Deliver, properties: BasicProperties, body: bytes) -> None:
+        """Subclasses should override this to handle incoming messages."""
+        pass
