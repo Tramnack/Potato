d9dc95524e9598ebc89904f742b2a987eafbd00d
Josef Rittiner <josef.rittiner@stud.hslu.ch>
2025-07-19 21:26:04 +0200

    Added temp files for testing Sphinx documentation generation

---
+++ services/temp_service.py
@@ -0,0 +1,16 @@
+"""
+A Temporary File for testing Sphinx Documentation
+"""
+
+def main(verbose=False):
+    """
+    :param verbose: Whether to print verbose output
+    :type verbose: bool
+    :return:
+    """
+    if verbose:
+        print('Hello World!')
+
+
+if __name__ == '__main__':
+    main(True)
