2af4aeff71fb1d8d2436443d60dfb51bc01e865c
Josef Rittiner <josef.rittiner@stud.hslu.ch>
2025-07-19 22:10:30 +0200

    Added new Mouth service. - Added simple console-out implementation

---
+++ services/mouth/console_out/app.py
@@ -0,0 +1,21 @@
+import os
+
+from services.shared_libs.RabbitMQ import RabbitMQConsumer
+
+
+def main():
+    RABBITMQ_HOST = os.getenv('RABBITMQ_HOST', 'localhost')
+    RABBITMQ_PORT = int(os.getenv('RABBITMQ_PORT', 5672))
+
+    consumer = RabbitMQConsumer(RABBITMQ_HOST, RABBITMQ_PORT)
+    print(' [*] Mouth waiting for messages. To exit press CTRL+C')
+    consumer.consume('brain_to_mouth', callback, auto_ack=True)
+
+
+def callback(ch, method, properties, body):
+    received_text = body.decode()
+    print(f" [x] Mouth received '{received_text}'")
+
+
+if __name__ == '__main__':
+    main()
