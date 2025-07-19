d0efe066e36d15b9bfb6ddf9a93808bc342497f1
Josef Rittiner <josef.rittiner@stud.hslu.ch>
2025-07-19 22:11:38 +0200

    Updated Echo-Brain - Echo-now forwards messages. - Added Mouth to docker-compose.yml

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 
 ## Features:
 Check the [services/template](services/template/README.md#steps) folder for info on adding a new Service.
+
+Check out the [Documentation](https://tramnack.github.io/Potato/index.html).
--- services/brain/echo/app.py
+++ services/brain/echo/app.py
@@ -1,21 +1,27 @@
 import os
 import sys
 
-from services.shared_libs.RabbitMQ import RabbitMQConsumer
+from services.shared_libs.RabbitMQ import RabbitMQConsumer, RabbitMQProducer
 
+RABBITMQ_HOST = os.getenv('RABBITMQ_HOST', 'localhost')
+RABBITMQ_PORT = int(os.getenv('RABBITMQ_PORT', 5672))
 
 def main():
-    RABBITMQ_HOST = os.getenv('RABBITMQ_HOST', 'localhost')
-    RABBITMQ_PORT = int(os.getenv('RABBITMQ_PORT', 5672))  # Also good to make port dynamic
 
     consumer = RabbitMQConsumer(RABBITMQ_HOST, RABBITMQ_PORT)
     print(' [*] Brain waiting for messages. To exit press CTRL+C')
-    consumer.consume('ear_to_brain', callback, auto_ack=True)
+    consumer.consume('ear_to_brain', callback, auto_ack=False)
 
 
 def callback(ch, method, properties, body):
     received_text = body.decode()
     print(f" [x] Brain received '{received_text}'")
+    processed_text = f"Brain echoed: {received_text}"  # Simple echo logic
+
+    producer = RabbitMQProducer(RABBITMQ_HOST, RABBITMQ_PORT)
+    producer.publish('brain_to_mouth', processed_text.encode())
+    print(f" [x] Brain sent '{processed_text}' to Mouth")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
 if __name__ == '__main__':
