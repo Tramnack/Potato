f9e5e4fe4d9c83187dd91b8ce1a707282aab5eae
Josef Rittiner <josef.rittiner@stud.hslu.ch>
2025-07-19 23:32:09 +0200

    Updated Ear Service - `AbstractEar` is now a RabbitMQProducer. - Updated `watchdog\app.py` to use `AbstractEar`

--- services/ear/abstract_ear.py
+++ services/ear/abstract_ear.py
@@ -1,5 +1,7 @@
 from abc import abstractmethod, ABC
 
+from services.shared_libs.RabbitMQ import RabbitMQProducer
 
-class AbstractEar(ABC):
+
+class AbstractEar(RabbitMQProducer, ABC):
     pass
--- services/ear/watchdog/app.py
+++ services/ear/watchdog/app.py
@@ -1,25 +1,33 @@
 import os
 import time
 
-from services.shared_libs.RabbitMQ import RabbitMQProducer
+from services.ear.abstract_ear import AbstractEar
 
+
+class WatchdogEar(AbstractEar):
+
+    def setup(self):
+        pass
+
+    def start_listening(self):
+        try:
+            i = 0
+            while True:
+                i += 1
+                user_input = f"Message Nr.{i}"
+                message = user_input.encode()
+                self.publish('ear_to_brain', message)
+                print(f" [x] Ear sent '{user_input}' to Brain")
+                time.sleep(1)
+        except KeyboardInterrupt:
+            pass
 
 def main():
     RABBITMQ_HOST = os.getenv('RABBITMQ_HOST', 'localhost')
     RABBITMQ_PORT = int(os.getenv('RABBITMQ_PORT', 5672))  # Also good to make port dynamic
 
-    producer = RabbitMQProducer(RABBITMQ_HOST, RABBITMQ_PORT)
-
-    try:
-        i = 0
-        while True:
-            i += 1
-            user_input = f"Message Nr.{i}"
-            message = user_input.encode()
-            producer.publish('ear_to_brain', message)
-            time.sleep(1)
-    except KeyboardInterrupt:
-        pass
+    producer = WatchdogEar(RABBITMQ_HOST, RABBITMQ_PORT)
+    producer.start_listening()
 
 
 if __name__ == '__main__':
