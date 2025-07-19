b16ffc267c930ec0b548e7d36f9978785c8a7945
Josef Rittiner <josef.rittiner@stud.hslu.ch>
2025-07-19 21:28:50 +0200

    Updated Sphinx conf.py to hopefully show both services and launcher

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../services'))
-sys.path.insert(0, os.path.abspath('../../launcher'))
+sys.path.insert(1, os.path.abspath('../../launcher'))
 
 project = 'Potato'
 copyright = '2025, Josef Rittiner'
