d89f7b0d5ce5f8437473937749ca9ce5029de441
Josef Rittiner <josef.rittiner@stud.hslu.ch>
2025-07-21 21:45:32 +0200

    Updated RabbitMQConsumer - Made updated for new AbstractRabbitMQ

--- services/shared_libs/RabbitMQ/RabbitMQConsumer.py
+++ services/shared_libs/RabbitMQ/RabbitMQConsumer.py
@@ -9,11 +9,11 @@ from services.shared_libs.RabbitMQ.AbstractRabbitMQ import AbstractRabbitMQ
 class RabbitMQConsumer(AbstractRabbitMQ, ABC):
 
     def consume(self, queue: str, auto_ack: bool = False):
-        self.channel.queue_declare(queue=queue, durable=True)
-        self.channel.basic_qos(prefetch_count=1)
-        self.channel.basic_consume(queue=queue, on_message_callback=self.callback,  auto_ack=auto_ack)
+        self._channel.queue_declare(queue=queue, durable=True)
+        self._channel.basic_qos(prefetch_count=1)
+        self._channel.basic_consume(queue=queue, on_message_callback=self.callback, auto_ack=auto_ack)
         print(f"Consuming messages from queue '{queue}'...")
-        self.channel.start_consuming()
+        self._channel.start_consuming()
 
     @abstractmethod
     def callback(self, ch: Channel, method: Basic.Deliver, properties: BasicProperties, body: bytes) -> None:
